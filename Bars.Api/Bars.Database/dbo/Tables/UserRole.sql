a72d0538bde1185754e8e8efd3069866527a9c01
Jackie Trillo <jackietrillo@hotmail.com>
2015-09-09 17:19:16 -0700

    Added User and UserRole tables

---
+++ Bars.Api/Bars.Database/dbo/Tables/UserRole.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[UserRoleId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [UserId]	 INT NOT NULL, 
+    [RoleId]	 INT NOT NULL,
+	CONSTRAINT [FK_UserRole_User] FOREIGN KEY (UserId) REFERENCES [dbo].[User] ([UserId]),
+	CONSTRAINT [FK_UserRole_Role] FOREIGN KEY (UserId) REFERENCES [dbo].[Role] ([RoleId])
+)
